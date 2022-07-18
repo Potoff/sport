50a5860ca4422d69a7d958357b1c93b61cb692f7
Paul Demarest <services.ankaamusic@gmail.com>
2022-07-18 14:52:47 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const models = require("./models");
+const getRoleMiddleware = require("./utils/getRoleMiddleware");
+
+mongoose.connect("mongodb://localhost/sportCenters", {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+});
+
+const app = express();
+
+app.set("models", models );
+
+const userRoute = require("./routes/user");
+
+app.use(express.json());
+app.use(getRoleMiddleware);
+userRoute(app);
+
+app.listen(3000, ()=>{
+    console.log("Server successfully launched")
+});
---
+++ routes/user.js
@@ -0,0 +1,21 @@
+const { userGet, userCreate, userUpdate, userDelete, userLogin } = require("../controllers/user");
+
+function userRoute(app) {
+    //Create
+    app.post("/userCreate", userCreate)
+
+    //Read
+    app.get("/users", userGet);
+
+    //Update
+    app.post("/userUpdate", userUpdate);
+
+    //Delete
+    app.post("/userDelete", userDelete);
+
+    //Login
+    app.post("/userLogin", userLogin)
+
+}
+
+module.exports = userRoute;
